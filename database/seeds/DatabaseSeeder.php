60728c68d600edad86f54d18852d497cc11af454
吴鑫 <wuxin@mail.weiba66.com>
2016-09-06 12:01:52 +0800

    no message

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -16,17 +16,12 @@ class DatabaseSeeder extends Seeder
 
             $this->call(RolesSeeder::class);
             $this->call(UsersSeeder::class);
-            $this->call(PostsSeeder::class);
-            $this->call(PostsMediaSeeder::class);
-            $this->call(CategoriesSeeder::class);
 
             DB::statement('SET FOREIGN_KEY_CHECKS=1;');
         }
         if (app()->environment() == 'production') {
             $this->call(RolesProductionSeeder::class);
             $this->call(UsersProductionSeeder::class);
-            $this->call(PostsProductionSeeder::class);
-            $this->call(CategoriesProductionSeeder::class);
 
             DB::statement('SET FOREIGN_KEY_CHECKS=1;');
         }
