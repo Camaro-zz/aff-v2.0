11822917e0683d0a64e44b0ae6d701dfa8f8b7ba
unknown <>
2016-09-12 22:16:33 +0800

    no message

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -16,6 +16,7 @@ class DatabaseSeeder extends Seeder
 
             $this->call(RolesSeeder::class);
             $this->call(UsersSeeder::class);
+            $this->call(SettingSeeder::class);
 
             DB::statement('SET FOREIGN_KEY_CHECKS=1;');
         }
